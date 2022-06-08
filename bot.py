10caaf1828a0c102b88c4afb9529e4c1e0402576
edux1 <edu.pm97@gmail.com>
2022-06-09 01:14:12 +0200

    Bot con las opciones de: - Poro - Decide

--- bot.py
+++ bot.py
@@ -1,15 +1,35 @@
 import os
-import discord
+import random
 
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+@bot.command(name='poro', help='Talk with Poro :)')
+async def poro(ctx):
+    poro_quotes = [
+        'Tengo hambre...',
+        'Grrrr!',
+        (
+            'Es hora de League of Legends! '
+            "Let's go"
+        ),
+        'Dame amor <3',
+        'Cookies!',
+        ':3'
+    ]
 
-client.run(TOKEN)
+    response = random.choice(poro_quotes)
+    await ctx.send(response)
+
+@bot.command(name='decide', help='Poro decides for you.')
+async def decide(ctx, *options: str):
+    decision = options[random.choice(range(0, len(options)))]
+    await ctx.send("Poro ha decidido que lo mejor es: " + decision)
+
+bot.run(TOKEN)
